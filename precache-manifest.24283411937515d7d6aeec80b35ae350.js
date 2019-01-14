4278ba46a491485ee9fd63d238792f54e5543750
Mike Gross <grossmi4@gmail.com>
2019-01-14 14:33:24 -0500

    Updates

---
+++ precache-manifest.24283411937515d7d6aeec80b35ae350.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a70c675f9a23daa38fda",
+    "url": "/clickygame/static/js/runtime~main.a70c675f.js"
+  },
+  {
+    "revision": "2a5c277b2d366d81164e",
+    "url": "/clickygame/static/js/main.2a5c277b.chunk.js"
+  },
+  {
+    "revision": "5ed6940b207ea9a11fe4",
+    "url": "/clickygame/static/js/1.5ed6940b.chunk.js"
+  },
+  {
+    "revision": "2a5c277b2d366d81164e",
+    "url": "/clickygame/static/css/main.32fb0ef8.chunk.css"
+  },
+  {
+    "revision": "79dbea1c9cf4e5df2003eb4f21d517d3",
+    "url": "/clickygame/index.html"
+  }
+];
